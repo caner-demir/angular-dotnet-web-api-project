7f752c75788c76049c16e7c1201bcab7ad88a326
Caner Demir <cnerdmir@gmail.com>
2021-09-08 00:58:46 +0300

    Create: movies-list component
    -Created toolbar component
    -Created rating component
    -Created generic-list component

--- angular-movies/src/app/app.component.ts
+++ angular-movies/src/app/app.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.moviesInTheaters = [{
+      title: "Spider Man",
+      releaseDate: new Date(),
+      price: 14.99,
+      poster: 'https://m.media-amazon.com/images/M/MV5BOGQ5YTM3YzctOTVmMC00OGIyLWFkZTYtMWYwOWZhMjA2MWMwXkEyXkFqcGdeQXVyMjUyMTE5MA@@._V1_FMjpg_UX1000_.jpg'
+    },
+    {
+      title: "Iron Man",
+      releaseDate: new Date('2016-11-14'),
+      price: 12.99,
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_FMjpg_UX1000_.jpg'
+    }];
+    this.moviesFutureReleases = [
+/*       {
+      title: "Avengers",
+      releaseDate: new Date(),
+      price: 14.49
+      },
+      {
+      title: "Batman",
+      releaseDate: new Date('2011-11-14'),
+      price: 12.99
+      } */
+    ];
+  }
+ 
+  moviesInTheaters;
+  moviesFutureReleases;
+
+  handleRating(rate: number){
+    alert(`The user selected ${rate}`);
+  }
 }
